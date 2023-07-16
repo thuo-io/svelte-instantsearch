3f874f89a2b737991aaed670294247fab81dc927
Jack Lee <jack.y.l.dev@gmail.com>
2023-07-16 03:01:42 -0700

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,26 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["svelte3", "@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", , "prettier"],
+  plugins: ["@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-  settings: {
-    "svelte3/typescript": () => require("typescript"),
-  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+  // settings: {
+  //   "svelte3/typescript": () => require("typescript"),
+  // },
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
   },
   env: {
     browser: true,
